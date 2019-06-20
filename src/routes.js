c2a08652b0741e5de43c78fdc5fd566581283706
Mubin <mbnmughal30@gmail.com>
2019-06-20 16:24:52 +0500

    404 route added.

--- src/routes.js
+++ src/routes.js
@@ -4,54 +4,57 @@ import Register from '@/views/Register';
 import Login from '@/views/Login';
 import Settings from '@/views/Settings';
 import SendInvite from '@/views/SendInvite';
+import NotFound from '@/views/404';
 
 const routes = [
-    {
-        path: '/',
-        component: SendInvite,
-        name: 'invite',
-        meta: {
-            requiresAuth: true,
-        }
-    },
-    {
-        path: '/patients',
-        component: Patients,
-        name: 'patients',
-        meta: {
-            requiresAuth: true,
-        }
-    },
-    {
-        path: '/settings',
-        component: Settings,
-        name: 'settings',
-        meta: {
-            requiresAuth: true,
-        }
-    },
-    {
-        path: '/login',
-        component: Login,
-        name: 'login',
-        props: true,
-        meta: {
-            requiresVisitor: true,
-        }
-    },
-    {
-        path: '/register',
-        component: Register,
-        name: 'register',
-        meta: {
-            requiresVisitor: true,
-        }
-    },
-    {
-        path: '/logout',
-        name: 'logout',
-        component: Logout
-    },
+  {
+    path: '/',
+    component: SendInvite,
+    name: 'invite',
+    meta: {
+      requiresAuth: true,
+    }
+  },
+  {
+    path: '/patients',
+    component: Patients,
+    name: 'patients',
+    meta: {
+      requiresAuth: true,
+    }
+  },
+  {
+    path: '/settings',
+    component: Settings,
+    name: 'settings',
+    meta: {
+      requiresAuth: true,
+    }
+  },
+  {
+    path: '/login',
+    component: Login,
+    name: 'login',
+    props: true,
+    meta: {
+      requiresVisitor: true,
+    }
+  },
+  {
+    path: '/register',
+    component: Register,
+    name: 'register',
+    meta: {
+      requiresVisitor: true,
+    }
+  },
+  {
+    path: '/logout',
+    name: 'logout',
+    component: Logout
+  },
+  { path: '/404', component: NotFound },
+  { path: '*', redirect: '/404' },
 ]
 
 export default routes
