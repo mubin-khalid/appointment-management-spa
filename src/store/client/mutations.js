7ed530dd41271599c074363bbd46b2fe18208b6d
Mubin <mbnmughal30@gmail.com>
2019-08-11 10:11:18 +0500

    add client module.

---
+++ src/store/client/mutations.js
@@ -0,0 +1,10 @@
+export default {
+  retrieveClients: (state, clients) => state.clients = clients,
+  totalClients: (state, total) => state.total = total,
+  deleteClient(state, index) {
+    state.clients.splice(index, 1)
+    state.total -= 1
+  },
+  addClient: (state, client) => state.clients.push(client),
+  editClient: (state, data) => state.clients[data.index].name = data.name,
+}
