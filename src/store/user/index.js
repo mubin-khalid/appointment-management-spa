c5fef81b40dec81b9a59e754d4e61f2f99e594b6
Mubin <mbnmughal30@gmail.com>
2019-08-11 10:12:43 +0500

    add user module.

---
+++ src/store/user/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import actions from './actions'
+import getters from './getters'
+import mutations from './mutations'
+import state from './state'
+
+
+export default {
+    namespaced: true,
+    actions, state, getters, mutations
+}
