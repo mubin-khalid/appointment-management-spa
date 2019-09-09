4809c1060e94b423e98eefef76faa5ad4e1a7e45
Mubin <mbnmughal30@gmail.com>
2019-09-09 19:54:30 +0500

    add language/tempalte component.

---
+++ src/views/admin/LT.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="w-full bg-white rounded px-1 py-3">
+    <ul class="flex border-b">
+      <li class="-mb-px mr-1">
+        <span class="bg-white inline-block py-2 px-4 font-semibold cursor-pointer"
+              :class="languageTab ? active : inactive"
+              @click="activateLanguages">
+        Languages</span>
+      </li>
+      <li class="-mb-px mr-1">
+        <span class="bg-white inline-block py-2 px-4 font-semibold cursor-pointer"
+              :class="templateTab ? active : inactive"
+              @click="activateTemplates"
+        >Templates</span>
+      </li>
+    </ul>
+    <language v-if="languageTab"></language>
+    <templates v-if="templateTab"></templates>
+  </div>
+</template>
+<script>
+  import Templates from './Templates'
+  import Language from './Language'
+
+  export default {
+    name: "lt",
+    data() {
+      return {
+        active: 'border-l border-t border-r rounded-t text-blue-700',
+        inactive: 'text-blue-500 hover:text-blue-800',
+        languageTab: true,
+        templateTab: false,
+      }
+    },
+    components: {
+      'templates': Templates,
+      'language': Language
+    },
+    created() {
+
+    },
+    methods: {
+      activateLanguages() {
+        this.languageTab = true;
+        this.templateTab = false;
+      },
+      activateTemplates() {
+        this.languageTab = false;
+        this.templateTab = true;
+      }
+    }
+  }
+</script>
