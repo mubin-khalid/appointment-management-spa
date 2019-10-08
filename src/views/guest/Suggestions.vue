3ebde5fb86b4db62f878f10ff56888678d47c9fb
Mubin <mbnmughal30@gmail.com>
2019-10-09 03:32:03 +0500

    remove 4th suggestion

--- src/views/guest/Suggestions.vue
+++ src/views/guest/Suggestions.vue
@@ -25,10 +25,6 @@
           <input type="date" class="bg-gray-400 rounded p-2" v-model="date3">
           <input type="text" class="bg-gray-400 rounded ml-2 p-2 h-10" v-model="time3">
         </div>
-        <div class="mb-3">
-          <input type="date" class="bg-gray-400 rounded p-2" v-model="date4">
-          <input type="text" class="bg-gray-400 rounded ml-2 p-2 h-10" v-model="time4">
-        </div>
         <button
           class="outline-none focus:outline-none bg-teal-500 hover:bg-teal-600 text-white font-bold p-2 rounded"
           @click="suggestNewTimings"
@@ -58,42 +54,60 @@
         show: false,
         suggestions: false,
         showSuggestions: false,
-        date1: new Date().getFullYear() + "-" + ('0' + (new Date().getMonth() + 1)).slice(-2) + '-' + ('0' +
-          new Date().getDate()).slice(-2),
-        time1: ("0" + new Date().getHours()).slice(-2) + ':' + ("0" + new Date().getMinutes()).slice(-2) +
-          ':00',
-        date2: new Date().getFullYear() + "-" + ('0' + (new Date().getMonth() + 1)).slice(-2) + '-' + ('0' +
-          new Date().getDate()).slice(-2),
-        time2: ("0" + new Date().getHours()).slice(-2) + ':' + ("0" + new Date().getMinutes()).slice(-2) +
-          ':00',
-        date3: new Date().getFullYear() + "-" + ('0' + (new Date().getMonth() + 1)).slice(-2) + '-' + ('0' +
-          new Date().getDate()).slice(-2),
-        time3: ("0" + new Date().getHours()).slice(-2) + ':' + ("0" + new Date().getMinutes()).slice(-2) +
-          ':00',
-        date4: new Date().getFullYear() + "-" + ('0' + (new Date().getMonth() + 1)).slice(-2) + '-' + ('0' +
-          new Date().getDate()).slice(-2),
-        time4: ("0" + new Date().getHours()).slice(-2) + ':' + ("0" + new Date().getMinutes()).slice(-2) +
-          ':00',
+        cachedDateTime: {
+          date1: null,
+          time1: null,
+          date2: null,
+          time2: null,
+          date3: null,
+          time3: null,
+        },
+        date1: null,
+        time1: null,
+        date2: null,
+        time2: null,
+        date3: null,
+        time3: null,
       }
     },
     created() {
-      this.get({
-        id: this.$route.params.id
-      }).then(response => {
-        this.suggestions = response.data.suggestions
-        this.cancelNote = response.data.verbiage.appointment_cancel_note
-        this.suggestButtonText = response.data.verbiage.suggest
-        if (!this.suggestions) {
+      this.cachedDateTime.date1 = this.date1 = new Date().getFullYear() + "-" +
+        ('0' + (new Date().getMonth() + 1)).slice(-2)
+        + '-' +
+        ('0' +
+          new Date().getDate()).slice(-2),
+        this.cachedDateTime.time1 = this.time1 = ("0" + new Date().getHours()).slice(-2) + ':' + ("0" +
+          new Date().getMinutes()).slice(-2) +
+          ':00',
+        this.cachedDateTime.date2 = this.date2 = new Date().getFullYear() + "-" +
+          ('0' + (new Date().getMonth() + 1)).slice(-2) + '-' + ('0' +
+            new Date().getDate()).slice(-2),
+        this.cachedDateTime.time2 = this.time2 = ("0" + new Date().getHours()).slice(-2) + ':' + ("0" +
+          new Date().getMinutes()).slice(-2) +
+          ':00',
+        this.cachedDateTime.date3 = this.date3 = new Date().getFullYear() + "-" +
+          ('0' + (new Date().getMonth() + 1)).slice(-2) + '-' + ('0' +
+            new Date().getDate()).slice(-2),
+        this.cachedDateTime.time3 = this.time3 = ("0" + new Date().getHours()).slice(-2) + ':' + ("0" +
+          new Date().getMinutes()).slice(-2) +
+          ':00',
+        this.get({
+          id: this.$route.params.id
+        }).then(response => {
+          this.suggestions = response.data.suggestions
+          this.cancelNote = response.data.verbiage.appointment_cancel_note
+          this.suggestButtonText = response.data.verbiage.suggest
+          if (!this.suggestions) {
+            this.$router.push({name: 'NotFound'})
+          }
+          if (typeof response.status !== 'undefined' && response.status == 'fail') {
+            this.suggested = true
+          } else {
+            this.suggested = false
+          }
+        }).catch(() => {
           this.$router.push({name: 'NotFound'})
-        }
-        if (typeof response.status !== 'undefined' && response.status == 'fail') {
-          this.suggested = true
-        } else {
-          this.suggested = false
-        }
-      }).catch(() => {
-        this.$router.push({name: 'NotFound'})
-      })
+        })
     },
     methods: {
       ...mapActions('appointment', [
@@ -102,7 +116,13 @@
       ]),
 
       suggestNewTimings() {
-        this.show = true
+        //this.show = true
+        if ((this.date1 == this.cachedDateTime.date1 && this.time1 == this.cachedDateTime.time1) ||
+          (this.date2 == this.cachedDateTime.date2 && this.time2 == this.cachedDateTime.time3) ||
+          (this.date3 == this.cachedDateTime.date3 && this.time3 == this.cachedDateTime.time3)) {
+          this.popup('Please fill in all of the suggested dates and times.', 'error', 3000)
+          return
+        }
         this.suggestTime({
           id: this.$route.params.id,
           cancelled_by: 'client',
@@ -112,8 +132,6 @@
           time2: this.time2,
           date3: this.date3,
           time3: this.time3,
-          date4: this.date4,
-          time4: this.time4
         }).then(() => {
           this.show = false
           this.suggested = true
