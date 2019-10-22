795bccb28e78441590cbf18ca2eb52721c16d961
Mubin <mbnmughal30@gmail.com>
2019-10-22 12:51:55 +0500

    revert to old date and new time.

--- src/views/guest/Suggestions.vue
+++ src/views/guest/Suggestions.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="appointment" class="m-auto mt-8 rounded w-full h-full text-center">
+  <div id="appointment" class="m-auto mt-8 rounded w-full text-center">
     <vue-element-loading :active="show" spinner="ring" is-full-screen color="#38b2ac"/>
     <transition name="fade" v-if="suggested">
       <div class="bg-white m-auto max-w-md rounded shadow-lg p-2">
         <span class="text-gray-700 font-medium">
-          Appointment cancelled. Suggestions have been sent to authority.
+          {{afterCancelNote}}
         </span>
       </div>
     </transition>
@@ -13,26 +13,20 @@
       >
         <div class="text-sm text-gray-700 mb-4">{{ cancelNote }}
         </div>
-        <div class="mb-3 w-5/6 m-auto">
-          <VueCtkDateTimePicker label="Suggestion 1" id="suggestion-1" :min-date='defaultDateTime'
-            v-model="date1" :no-button-now="true" formatted="YYYY-MM-DD HH:mm"
-                                :no-label="false"
-                                :first-day-of-week="1"
-                                format="'YYYY-MM-DD HH:mm'"></VueCtkDateTimePicker>
+        <div class="mb-3">
+          <input type="date" class="bg-gray-400 rounded p-1-5 outline-none focus:outline-none cursor-pointer"
+                 v-model="date1" :min="cachedDateTime.date1">
+          <vue-timepicker type="text" class="md:ml-2" v-model="time1"></vue-timepicker>
         </div>
-        <div class="mb-3 w-5/6 m-auto">
-          <VueCtkDateTimePicker label="Suggestion 2" id="suggestion-2" :min-date='defaultDateTime'
-                                v-model="date2" :no-button-now="true" formatted="YYYY-MM-DD HH:mm"
-                                :no-label="false"
-                                :first-day-of-week="1"
-                                format="'YYYY-MM-DD HH:mm'"></VueCtkDateTimePicker>
+        <div class="mb-3">
+          <input type="date" class="bg-gray-400 rounded p-2 outline-none focus:outline-none cursor-pointer"
+                 v-model="date2" :min="cachedDateTime.date2">
+          <vue-timepicker type="text" class="md:ml-2" v-model="time2"></vue-timepicker>
         </div>
-        <div class="mb-3 w-5/6 m-auto">
-          <VueCtkDateTimePicker label="Suggestion 3" id="suggestion-3" :min-date='defaultDateTime'
-                                v-model="date3" :no-button-now="true" formatted="YYYY-MM-DD HH:mm"
-                                :no-label="false"
-                                :first-day-of-week="1"
-                                format="'YYYY-MM-DD HH:mm'"></VueCtkDateTimePicker>
+        <div class="mb-3">
+          <input type="date" class="bg-gray-400 rounded p-2 outline-none focus:outline-none cursor-pointer"
+                 v-model="date3" :min="cachedDateTime.date3">
+          <vue-timepicker type="text" class="md:ml-2 sm:mt-2 lg:mt-0" v-model="time3"></vue-timepicker>
         </div>
         <button
           class="outline-none focus:outline-none bg-teal-500 hover:bg-teal-600 text-white font-bold p-2 rounded"
@@ -46,30 +40,34 @@
 
 <script>
   import Popup from '@/mixins/Popup'
-  import {mapActions} from 'vuex'
+  import {mapActions, mapGetters} from 'vuex'
   import VueElementLoading from 'vue-element-loading'
-  import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
-  import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
+  import VueTimePicker from 'vue2-timepicker'
+  import 'vue2-timepicker/dist/VueTimepicker.css'
 
   export default {
     name: "Suggestions",
     mixins: [Popup],
     components: {
       VueElementLoading,
-      'VueCtkDateTimePicker': VueCtkDateTimePicker
+      'vue-timepicker': VueTimePicker
     },
+    computed: {},
     data() {
       return {
         cancelNote: 'Note: Appointment will be cancelled automatically after suggestions.',
         suggestButtonText: 'Suggest',
+        afterCancelNote: '',
         suggested: false,
         error: false,
         show: false,
+        time1Class: '',
+        time2Class: '',
+        time3Class: '',
         timeRegex: /^([0-1][0-9]|2[0-3]):([0-5][0-9])$/i,
         suggestions: false,
-        defaultDateTime: '',
         showSuggestions: false,
-        suggestedDateTime: {
+        cachedDateTime: {
           date1: null,
           time1: null,
           date2: null,
@@ -78,8 +76,11 @@
           time3: null,
         },
         date1: null,
+        time1: null,
         date2: null,
+        time2: null,
         date3: null,
+        time3: null,
       }
     },
     created() {
@@ -87,14 +88,16 @@
       let day = currentDate.getDate() >= 10 ? currentDate.getDate() : '0' + currentDate.getDate()
       let month = currentDate.getMonth() + 1 >= 10 ? currentDate.getMonth() + 1 : '0' + currentDate.getMonth() + 1
       let year = currentDate.getFullYear()
-      this.defaultDateTime = `${year}-${month}-${day} 00:00:00`
-      
+      this.cachedDateTime.date1 =
+        this.cachedDateTime.date2 =
+          this.cachedDateTime.date3 = `${year}-${month}-${day}`
       this.get({
         id: this.$route.params.id
       }).then(response => {
         this.suggestions = response.data.suggestions
         this.cancelNote = response.data.verbiage.appointment_cancel_note
-        this.suggestButtonText = response.data.verbiage.suggest
+        this.suggestButtonText = response.data.verbiage.send
+        this.afterCancelNote = response.data.verbiage.aftercancelheading
         if (!this.suggestions) {
           this.$router.push({name: 'NotFound'})
         }
@@ -107,6 +110,35 @@
         this.$router.push({name: 'NotFound'})
       })
     },
+    watch: {
+      // time1(newTime) {
+      //   if (!this.timeRegex.test(newTime)) {
+      //     this.time1Class = 'border border-red-600'
+      //     this.error = true
+      //   } else {
+      //     this.error = false
+      //     this.time1Class = 'border-0'
+      //   }
+      // },
+      // time2(newTime) {
+      //   if (!this.timeRegex.test(newTime)) {
+      //     this.time2Class = 'border border-red-600'
+      //     this.error = true
+      //   } else {
+      //     this.error = false
+      //     this.time2Class = 'border-0'
+      //   }
+      // },
+      // time3(newTime) {
+      //   if (!this.timeRegex.test(newTime)) {
+      //     this.time3Class = 'border border-red-600'
+      //     this.error = true
+      //   } else {
+      //     this.error = false
+      //     this.time3Class = 'border-0'
+      //   }
+      // }
+    },
     methods: {
       ...mapActions('appointment', [
         'get',
@@ -116,7 +148,12 @@
       suggestNewTimings() {
 
         if (this.date1 == null) {
-          this.popup('Please fill in at least first Suggestion.', 'error', 3000)
+          this.popup('Please fill in at least first date.', 'error', 3000)
+          return
+        }
+        if (this.time1 == null || this.time1.HH == "" ||
+          this.time1.mm == "") {
+          this.popup('Select proper time.', 'error', 3000)
           return
         }
 
@@ -125,29 +162,15 @@
           return
         }
         this.show = true
-        let suggestion1 = this.date1.slice(1, -1).split(' ')
-        this.suggestedDateTime.date1 = suggestion1[0] 
-        this.suggestedDateTime.time1 = suggestion1[1] 
-        if(this.date2 != null) {
-          let suggestion2 = this.date2.slice(1, -1).split(' ')
-          this.suggestedDateTime.date2 = suggestion2[0]
-          this.suggestedDateTime.time2 = suggestion2[1]
-        }
-        
-        if(this.date3 != null) {
-          let suggestion3 = this.date3.slice(1, -1).split(' ')
-          this.suggestedDateTime.date3 = suggestion3[0]
-          this.suggestedDateTime.time3 = suggestion3[1]
-        }
         this.suggestTime({
           id: this.$route.params.id,
           cancelled_by: 'client',
-          date1: this.suggestedDateTime.date1,
-          time1: this.suggestedDateTime.time1,
-          date2: this.suggestedDateTime.date2,
-          time2: this.suggestedDateTime.time2,
-          date3: this.suggestedDateTime.date3,
-          time3: this.suggestedDateTime.time3,
+          date1: this.date1,
+          time1: (this.time1 != null) ? Object.values(this.time1).join(':') : null,
+          date2: this.date2,
+          time2: (this.time2 != null) ? Object.values(this.time2).join(':') : null,
+          date3: this.date3,
+          time3: (this.time3 != null) ? Object.values(this.time3).join(':') : null,
         }).then(() => {
           this.show = false
           this.suggested = true
@@ -186,5 +209,9 @@
 
   .w-brief {
     widht: 480px;
+  }
+
+  .p-1-5 {
+    padding: .35rem;
   }
 </style>
