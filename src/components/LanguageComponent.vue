0748e0abd1e05dfc09c5e41fd6b30251c029de7f
Mubin <mbnmughal30@gmail.com>
2019-10-23 22:11:19 +0500

    reset langauges once booking made.

--- src/components/LanguageComponent.vue
+++ src/components/LanguageComponent.vue
@@ -3,8 +3,9 @@
     class="block appearance-none w-full bg-gray-200 border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
     id="language"
     @change="languageChanged($event)"
+    v-model="languageIndex"
   >
-    <option value="0" selected disabled>Please Select</option>
+    <option value="0" disabled>Please Select</option>
     <option v-for="language in languages" :key="language.id" :id="language.id" :value="language.id">{{
       language.language
       }}
@@ -17,7 +18,15 @@
 
   export default {
     name: "LanguageComponent",
+    data() {
+      return {
+        languageIndex: 0
+      }
+    },
     created() {
+      eventBus.$on('resetLanguageComponent', () => {
+        this.languageIndex = 0
+      })
       this.getLanguages()
     },
     computed: {
